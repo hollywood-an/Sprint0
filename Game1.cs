f6fa48c95d7de54cbc8f79b7bbf3580b52602bc7
Frank An <anfrank2015@gmail.com>
2026-09-11 02:52:14 -0400

    Wrote Game1 to tie everything together

--- Game1.cs
+++ Game1.cs
@@ -1,51 +1,100 @@
-﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Sprint0.Commands;
+using Sprint0.Controllers;
+using Sprint0.Interfaces;
+using Sprint0.Players;
 
 namespace Sprint0;
 
 public class Game1 : Game
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
+    private const int ScreenWidth = 800;
+    private const int ScreenHeight = 600;
+    private const int PlayerPixelSize = 64;
+    private static readonly Color GrassGreen = new Color(58, 110, 66);
+
+    private readonly GraphicsDeviceManager graphics;
+    private SpriteBatch spriteBatch;
+    private IPlayer player;
+    private List<IController> controllers;
 
     public Game1()
     {
-        _graphics = new GraphicsDeviceManager(this);
+        graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
+        graphics.PreferredBackBufferWidth = ScreenWidth;
+        graphics.PreferredBackBufferHeight = ScreenHeight;
+        graphics.ApplyChanges();
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        Texture2D spriteSheet;
+        using (Stream stream = TitleContainer.OpenStream("Content/adventurer.png"))
+        {
+            spriteSheet = Texture2D.FromStream(GraphicsDevice, stream);
+        }
+
+        Rectangle screenBounds = GraphicsDevice.Viewport.Bounds;
+        Vector2 screenCenter = new Vector2((ScreenWidth - PlayerPixelSize) / 2f, (ScreenHeight - PlayerPixelSize) / 2f);
+        player = new Player(spriteSheet, screenCenter, screenBounds);
+
+        controllers = new List<IController>
+        {
+            BuildKeyboardController(),
+            new MouseController(this, player, new StopCommand(player))
+        };
+    }
+
+    private IController BuildKeyboardController()
+    {
+        KeyboardController keyboard = new KeyboardController();
+
+        ICommand moveUp = new MoveCommand(player, Direction.Up);
+        ICommand moveDown = new MoveCommand(player, Direction.Down);
+        ICommand moveLeft = new MoveCommand(player, Direction.Left);
+        ICommand moveRight = new MoveCommand(player, Direction.Right);
+        keyboard.RegisterHeldCommand(Keys.W, moveUp);
+        keyboard.RegisterHeldCommand(Keys.S, moveDown);
+        keyboard.RegisterHeldCommand(Keys.A, moveLeft);
+        keyboard.RegisterHeldCommand(Keys.D, moveRight);
+
+        ICommand quit = new QuitCommand(this);
+        keyboard.RegisterPressedCommand(Keys.Space, new DashCommand(player));
+        keyboard.RegisterPressedCommand(Keys.Q, quit);
+        keyboard.RegisterPressedCommand(Keys.Escape, quit);
+
+        return keyboard;
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
-        // TODO: Add your update logic here
-
+        foreach (IController controller in controllers)
+        {
+            controller.Update();
+        }
+        player.Update(gameTime);
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // TODO: Add your drawing code here
-
+        GraphicsDevice.Clear(GrassGreen);
+        spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp);
+        player.Draw(spriteBatch);
+        spriteBatch.End();
         base.Draw(gameTime);
     }
 }
